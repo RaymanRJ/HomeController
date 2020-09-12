dca308345af6a910aac9767c03e6f5fb7f2cd3af
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2020-09-12 10:09:45 -0400

    sbt assembly works! (#3)

--- src/PiController/src/main/scala/Main.scala
+++ src/PiController/src/main/scala/Main.scala
@@ -1,4 +1,11 @@
+import java.lang.Thread.sleep
+
 object Main extends App {
+
+  while(true){
+    println("I'm running, running ..")
+    sleep(5000)
+  }
   val ages = Seq(42, 75, 29, 64)
   println(s"The oldest person is ${ages.max}")
 }
