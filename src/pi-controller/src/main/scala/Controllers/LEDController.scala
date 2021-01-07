4eac066367ea828339376455a8628a7799c9a526
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2021-01-07 01:24:25 -0500

    WIP - Testing LED controller build (#8)
    Pi can control 256 LEDs (external power). But they are running in 4 groups of 64, all parallel. This means the pi only addresses 64 LEDs, and all four branches behave the exact same.

--- src/pi-controller/src/main/scala/Controllers/LEDController.scala
+++ src/pi-controller/src/main/scala/Controllers/LEDController.scala
@@ -2,22 +2,30 @@ package Controllers
 
 import Interfaces.IController
 import com.github.mbelling.ws281x.{Color, LedStripType, Ws281xLedStrip}
+import Messaging.LEDSequence
 
-case class LEDController() extends IController {
+trait LEDControllerTraits{
+  var ledStrip: Ws281xLedStrip
+  var ledSequence: LEDSequence.Value
+  var timing: Int
+  var color: Color
+}
+
+case class LEDController() extends IController with LEDControllerTraits{
+
+  override var ledSequence: LEDSequence.Value = LEDSequence.SOLID_COLOUR
+  override var timing: Int = -1
+  override var color: Color = Color.GREEN
+  override var ledStrip = new Ws281xLedStrip(
+    16, 18, 800 * 1000,
+    10, 255, 0,
+    false, LedStripType.WS2811_STRIP_GRB, true
+  )
 
   override def start(): Unit = {
-
     println("Starting LEDController")
-
-    val ledStrip = new Ws281xLedStrip(
-      16, 18, 800 * 1000, 10, 255, 0, false, LedStripType.WS2811_STRIP_GRB, true
-    )
-
-    ledStrip.setPixel(0, Color.RED)
-    ledStrip.setPixel(1, Color.GREEN)
-    ledStrip.setPixel(2, Color.BLUE)
+    ledStrip.setStrip(color)
     ledStrip.render()
-
 
     Thread.sleep(5 * 1000)
 
