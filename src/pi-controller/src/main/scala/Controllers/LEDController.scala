89909d09124f0ceb697bb974e0d48fb07abb299d
raymanrj <raymanrj@live.com>
2021-01-09 19:58:05 -0500

    WIP - testing multiple pins on PI

--- src/pi-controller/src/main/scala/Controllers/LEDController.scala
+++ src/pi-controller/src/main/scala/Controllers/LEDController.scala
@@ -5,7 +5,7 @@ import com.github.mbelling.ws281x.{Color, LedStripType, Ws281xLedStrip}
 import Messaging.LEDSequence
 
 trait LEDControllerTraits{
-  var ledStrip: Ws281xLedStrip
+  var ledStrips: List[Ws281xLedStrip]
   var ledSequence: LEDSequence.Value
   var timing: Int
   var color: Color
@@ -15,17 +15,29 @@ case class LEDController() extends IController with LEDControllerTraits{
 
   override var ledSequence: LEDSequence.Value = LEDSequence.SOLID_COLOUR
   override var timing: Int = -1
-  override var color: Color = Color.GREEN
-  override var ledStrip = new Ws281xLedStrip(
-    16, 18, 800 * 1000,
-    10, 255, 0,
-    false, LedStripType.WS2811_STRIP_GRB, true
+  override var color: Color = Color.BLUE
+  override var ledStrips = List(
+    new Ws281xLedStrip(
+      16, 18, 800 * 1000,
+      10, 255, 0,
+      false, LedStripType.WS2811_STRIP_GRB, true
+    ),
+    new Ws281xLedStrip(
+      16, 12, 800 * 1000,
+      10, 255, 0,
+      false, LedStripType.WS2811_STRIP_GRB, true
+    )
   )
 
   override def start(): Unit = {
     println("Starting LEDController")
-    ledStrip.setStrip(color)
-    ledStrip.render()
+    ledStrips.foreach(s => {
+      s.setStrip(color)
+      s.render()
+    })
+
+    ledStrips.head.setStrip(Color.WHITE)
+    ledStrips.head.render()
 
     Thread.sleep(5 * 1000)
 
