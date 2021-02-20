dcd53c79c0450a32e744fae3d4ccbbf756a8171e
raymanrj <raymanrj@live.com>
2021-02-20 13:51:00 -0500

    Added dockerfile

--- src/web-server/src/Main.py
+++ src/web-server/src/Main.py
@@ -1,17 +1,13 @@
 from flask import Flask
 
-from src.SmartDevices.IRaspberryPi import IRaspberryPi
-from src.Utilities.JSONify import toJSON
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    device = IRaspberryPi(ip_address='192.168.0.1', port=22)
     return {
-        "response": 'Hello, World!',
-        "device": toJSON(device)
+        "response": 'Hello, World!'
     }
 
 
